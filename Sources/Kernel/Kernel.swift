99b6de85aed9c7d1aee71764df72b20c1295e85d
RandomHashTags <imrandomhashtags@gmail.com>
2026-02-03 06:43:00 -0600

    add swift code that compiles using simd, causing linking errors due to them not yet being supported

--- Sources/Kernel/Kernel.swift
+++ Sources/Kernel/Kernel.swift
@@ -11,6 +11,19 @@ public func kmain(
     UART.initialize()
     unsafe initKernel(infoPointer: infoPointer)
 
+    logger.log("kmain: creating set...")
+    var set = Set<Int>()
+    logger.log("kmain: set created")
+
+    logger.log("kmain: inserting 1 into set...")
+    set.insert(1)
+    logger.log("kmain: inserted 1 into set")
+    if set.contains(1) {
+        logger.log("set.contains(1)")
+    } else {
+        logger.log("!set.contains(1)")
+    }
+
     logger.log("kmain: halting CPU")
     while true {
         cpu_halt()
