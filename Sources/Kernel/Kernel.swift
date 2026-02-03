05b4d1bda9a804b357435f0d1a9a51a1e143f335
RandomHashTags <imrandomhashtags@gmail.com>
2026-02-03 08:05:55 -0600

    enable SIMD

--- Sources/Kernel/Kernel.swift
+++ Sources/Kernel/Kernel.swift
@@ -11,21 +11,32 @@ public func kmain(
     UART.initialize()
     unsafe initKernel(infoPointer: infoPointer)
 
-    logger.log("kmain: creating set...")
-    var set = Set<Int>()
-    logger.log("kmain: set created")
-
-    logger.log("kmain: inserting 1 into set...")
-    set.insert(1)
-    logger.log("kmain: inserted 1 into set")
-    if set.contains(1) {
-        logger.log("set.contains(1)")
-    } else {
-        logger.log("!set.contains(1)")
-    }
+    testSIMD()
 
     logger.log("kmain: halting CPU")
     while true {
         cpu_halt()
     }
+}
+
+func testSIMD() {
+    logger.log("testSIMD: testing...")
+    let bro = UnsafeMutableRawPointer.allocate(byteCount: 1024, alignment: 16)
+    unsafe bro.initializeMemory(as: UInt64.self, repeating: 3, count: 16)
+    unsafe clearMemory(at: bro, count: 1024)
+    logger.log("testSIMD: success")
+
+    //unsafe bro.deallocate() // TODO: fix | causes reboots
+}
+func clearMemory(
+    at address: UnsafeMutableRawPointer,
+    count: Int
+) {
+    logger.log("clearMemory: executing...")
+    let zero = SIMD64<UInt8>(repeating: 0) // 512-bit vector
+    let pointer = unsafe address.assumingMemoryBound(to: SIMD64<UInt8>.self)
+    for i in 0..<(count / 64) {
+        unsafe pointer[i] = zero
+    }
+    logger.log("clearMemory: success")
 }
