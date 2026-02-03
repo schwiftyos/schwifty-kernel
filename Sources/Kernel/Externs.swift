00ee9f95bbdf5d08fed16b5ca940ebb69cd4b474
RandomHashTags <imrandomhashtags@gmail.com>
2026-02-03 10:42:15 -0600

    only enable sse, avx and avx512 if available - will enable legacy registers and AMX in a later commit

--- Sources/Kernel/Externs.swift
+++ Sources/Kernel/Externs.swift
@@ -9,3 +9,24 @@ func outb(_ port: UInt16, _ value: UInt8)
 
 @_extern(c, "inb")
 func inb(_ port: UInt16) -> UInt8
+
+// MARK: cpuid
+@_extern(c, "cpuid_low")
+private func cpuid_low(
+    leaf: UInt32,
+    subleaf: UInt32,
+    result: UnsafeMutableRawPointer
+)
+
+func cpuidLow(leaf: UInt32, subleaf: UInt32, result: inout CPUIDResult) {
+    //logger.log("cpuidLow: calling cpuid_low...")
+    unsafe cpuid_low(leaf: leaf, subleaf: subleaf, result: &result)
+    //logger.log("cpuidLow: loaded cpuid_low value")
+}
+
+struct CPUIDResult {
+    var eax:UInt32 = 0
+    var ebx:UInt32 = 0
+    var ecx:UInt32 = 0
+    var edx:UInt32 = 0
+}
